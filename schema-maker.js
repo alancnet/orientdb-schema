3f47d605dbed3a0c0e81758d22b8e0ff521aef12
Alan Colon <acolon@lifetimefitness.com>
2017-05-10 21:13:45 -0700

    Support for indexes

--- schema-maker.js
+++ schema-maker.js
@@ -1,21 +1,21 @@
 const _ = require('lodash')
-const {types} = require('orientjs');
+const {types} = require('orientjs')
 
 const diffList = (a, b) => ({
   common: a.map((x) => [x].concat(
-    b.filter((y) => _.matches({x})({x:y}))
+    b.filter((y) => _.matches({x})({x: y}))
   ))
     .filter((x) => x.length - 1),
-    //.reduce((a,b) => a.concat(b), []),
+    // .reduce((a,b) => a.concat(b), []),
   left: a.map((x) => ({x})).filter((z) => !b.filter((x) => _.matches(z)({x})).length).map(({x}) => x),
   right: b.map((x) => ({x})).filter((z) => !a.filter((x) => _.matches(z)({x})).length).map(({x}) => x)
 })
 
 const diffListRef = (a, b, get) => {
-  const cs = (a.length > b.length ? a : b).map(()=>({}));
+  const cs = (a.length > b.length ? a : b).map(() => ({}))
   const ax = a.map((x, i) => [get(x), cs[i]])
   const bx = b.map((x, i) => [get(x), cs[i]])
-  const diff = diffList(ax,bx)
+  const diff = diffList(ax, bx)
   return {
     common: diff.common.map((group) =>
       group.slice(0, 1).map(([x, i]) => a[cs.indexOf(i)])
@@ -26,15 +26,43 @@ const diffListRef = (a, b, get) => {
   }
 }
 
-const isBuiltIn = (x) => x === 'V' || x === 'E' || x[0] === '_' || x[0] === 'O';
+const isBuiltIn = (x) => x === 'V' || x === 'E' || x[0] === '_' || x[0] === 'O'
+const builtInIndexes = [
+  'OFunction.name',
+  'dictionary',
+  'ORole.name',
+  'OUser.name'
+]
+const isBuiltInIndex = (x) => builtInIndexes.includes(x)
 
-const diffSchemas = (db, targetSchema) => db.class.list().then((existingClasses) => {
+const diffSchemas = (db, targetSchema) => Promise.all([
+  db.class.list(),
+  db.index.list().then((existingIndexes) =>
+    existingIndexes
+      .filter((x) => !isBuiltInIndex(x.name))
+      .map((x) => ({
+        name: x.name,
+        type: x.type,
+        class: x.definition && x.definition.className,
+        properties: (x.definition && x.definition.field) ? [x.definition.field]
+          : (x.definition && x.definition.indexDefinitions || []).map((d) => (d.field))
+      }))
+  )
+]).then(([existingClasses, existingIndexes]) => {
   const classesDiff = diffListRef(existingClasses, targetSchema, (x) => x.name)
+  const targetIndexes = targetSchema.map((c) => (c.indexes || []).map((index) =>
+    Object.assign({}, index, {class: c.name, type: index.type.toUpperCase()})
+  ))
+    .reduce((a, b) => a.concat(b), [])
+
+  const indexesDiff = diffListRef(existingIndexes, targetIndexes, (x) =>
+    `${x.name},${x.type},${x.class},${x.properties.join(';')}`
+  )
   return {
     newClasses: classesDiff.right,
     extraClasses: classesDiff.left
       .filter((x) => !isBuiltIn(x.name)),
-    existingClasses: classesDiff.common.map(([currentClass,newClass]) => ({
+    existingClasses: classesDiff.common.map(([currentClass, newClass]) => ({
       name: currentClass.name,
       class: currentClass,
       properties: diffListRef(
@@ -45,14 +73,23 @@ const diffSchemas = (db, targetSchema) => db.class.list().then((existingClasses)
           type: types[x.type] || x.type
         })
       )
-    }))
+    })),
+    newIndexes: indexesDiff.right,
+    extraIndexes: indexesDiff.left,
+    existingIndexes: indexesDiff.common
   }
 })
 
 const schemaMaker = (db, targetSchema) =>
   diffSchemas(db, targetSchema)
     .then((diff) => {
-      var steps = [];
+      var steps = []
+      diff.extraIndexes.forEach((i) => {
+        steps.push({
+          description: `Delete index ${i.name}`,
+          action: () => db.index.drop(i.name)
+        })
+      })
       diff.extraClasses.forEach((c) => {
         steps.push({
           description: `Delete class ${c.name}.`,
@@ -64,16 +101,17 @@ const schemaMaker = (db, targetSchema) =>
           description: `Create class ${c.name}` +
             (c.superClass ? `, with superClass ${c.superClass}` : ``) +
             (c.properties && c.properties.length ? `, with properties: ` : ``) +
-            (c.properties||[]).map((prop) => `${prop.type} ${prop.name}`).join(', ') +
+            (c.properties || []).map((prop) => `${prop.type} ${prop.name}`).join(', ') +
             '.',
           action: () => {
-            console.log('Creating class ', c.name, ':', c.superClass);
+            console.log('Creating class ', c.name, ':', c.superClass)
             return db.class.create(c.name, c.superClass)
               .then((cl) => {
-                if (c.properties && c.properties.length)
+                if (c.properties && c.properties.length) {
                   return cl.property.create(c.properties)
-                else
-                  return Promise.resolve();
+                } else {
+                  return Promise.resolve()
+                }
               }, console.error)
               .catch((err) => console.error(err))
           }
@@ -98,24 +136,31 @@ const schemaMaker = (db, targetSchema) =>
         })
       })
 
+      diff.newIndexes.forEach((i) => {
+        steps.push({
+          description: `Create index ${i.name} (${i.properties.join(', ')}) ${i.type}`,
+          action: () =>
+            db.index.create(i)
+        })
+      })
+
       return {
         _diff: diff,
         explain: () => steps.map((x) => x.description).join('\n'),
         execute: () => {
-          const queue = steps.slice(0);
+          const queue = steps.slice(0)
           const next = () => {
             if (queue.length) {
-              const step = queue.shift();
-              console.log("Executing: ", step.description);
+              const step = queue.shift()
+              console.log('Executing: ', step.description)
               return step.action().then(() => next())
             } else {
-              return Promise.resolve("Done");
+              return Promise.resolve('Done')
             }
           }
-          return next();
+          return next()
         }
       }
     })
-
 
 module.exports = schemaMaker
